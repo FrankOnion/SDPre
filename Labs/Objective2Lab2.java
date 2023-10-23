8460ed0cd45c31344acb0c85e34325ffc4e4da9a
FrankOnion <lanaethonis@tuta.io>
2023-10-23 08:04:55 -0700

    Objective2Lab2 complete

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Michael";
+    int age = 40;
+    String state = "Texas";
+
+   System.out.println("name: " + name + "\nage: " + age + "\nstate: " + state);   
+  }
+}
