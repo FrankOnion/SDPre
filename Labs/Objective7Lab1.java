ef1be7154c00fa8a55be3a8c946d6ba04740a134
FrankOnion <lanaethonis@tuta.io>
2023-10-23 13:52:50 -0400

    Objective 7 Lab 1 Complete

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,14 @@
+public class Objective7Lab1 {
+	public static void main(String[] args) {
+		int counter = 10;
+
+		while(counter > 0) {
+
+			System.out.println(counter);
+
+			//Not sure what should go here since the example code seems to do exactly what is asked
+
+			counter = counter - 1;
+		}
+	}
+}
