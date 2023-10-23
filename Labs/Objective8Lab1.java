3f553a303c25462f6d81c59507e54f95c7cec232
FrankOnion <lanaethonis@tuta.io>
2023-10-23 16:05:03 -0400

    Objective 8 Lab 1 complete

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+		//TODO Write a for loop that will print the numbers 1 to 10
+
+		for(int num = 1; num <= 10; num++){
+			System.out.println(num);
+		}
+
+		//TODO Write another for loop that will print the numbers 10 to 1
+		
+		for(int num = 10; num >= 1; num--){
+			System.out.println(num);
+
+		}
+	}
+}
